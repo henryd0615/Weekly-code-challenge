83f694172375e0d47865937b35cd2bb57e0d62de
henryd0615 <henryd0615@gmail.com>
2022-03-18 01:10:57 -0400

    Edit

--- src/com/revature/main/challenge.java
+++ src/com/revature/main/challenge.java
@@ -7,26 +7,22 @@ public class challenge {
 
 	public static void main(String[] args) {
 		
-		//Create LinkedList one
 		LinkedList<Integer> one = new LinkedList<>(); 
 		one.add(3); 
 		one.add(7); 
 		one.add(8); 
 		one.add(10);
 
-		//Create LinkedList two
 		LinkedList<Integer> two = new LinkedList<>(); 
 		two.add(99); 
 		two.add(1); 
 		two.add(8); 
 		two.add(10); 
 		
-		//Iterate through LinkedList one 
 		for(int each : one) {
-			//compare with LinkedList two
 			if(two.contains(each)) {
 				System.out.println(each);
-				return; //break at first equivalent value
+				return; 
 			}
 				
 		}
